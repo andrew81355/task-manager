da2e47bd06f2219fed524479eee3bc835f30e387
Alireza Mahmoudi <ali.reza.mahmoud@gmail.com>
2024-09-09 19:38:11 +0200

    Backend - eslint configuration for supporting vitest global

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,8 +1,19 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import vitestPlugin from "eslint-plugin-vitest";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                ...vitestPlugin.environments.env.globals
+            },
+        },
+    },
+
+    pluginJs.configs.recommended,
+    // Add Vitest recommended configuration
+    vitestPlugin.configs.recommended
 ];
